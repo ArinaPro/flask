e75073382aaaf987509ea69a6c1888dbe8189f03
coolstar.arina@gmail.com <eparis@atlassian.com>
2023-10-24 20:31:25 +0300

    leson17.10: add tamplates leson23.10: add context

--- app1.py
+++ app1.py
@@ -1,12 +1,27 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from dotenv import load_dotenv
+from enums import MAX_SCORE, NAME, students
+
+load_dotenv()
 
 app =Flask(__name__)
 
 
 @app.route('/')
 def welcom():
-    return "This is the main page of Arina"
+    return render_template("index.html")
+
+@app.route("/context")
+def context():
+    context = {
+        "title" : "Python Course",
+        "name" : NAME,
+        "max_score" : MAX_SCORE,
+        "students" : students
+    }
+    return render_template("context.html", **context)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=os.getenv("DEBUG"))
